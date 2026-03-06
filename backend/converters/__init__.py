584279e2f74f26e8a1e85438006b4abf97c9d970
Chase Roohms <chaseroohms@gmail.com>
2026-03-06 15:33:25 -0600

    feat: add LibreOfficeConverter for presentation format conversions and include sample PPT/PPTX files

--- backend/converters/__init__.py
+++ backend/converters/__init__.py
@@ -6,6 +6,7 @@ from .pypandoc_convert import PyPandocConverter
 from .pymupdf_convert import PyMuPDFConverter
 from .pysubs2_convert import PySubs2Converter
 from .fonttools_convert import FonttoolsConverter
+from .libreoffice_convert import LibreOfficeConverter
 from .converter_interface import ConverterInterface
 
-__all__ = ["FFmpegConverter", "PillowConverter", "PandasConverter", "DrawioConverter", "PyPandocConverter", "PyMuPDFConverter", "PySubs2Converter", "FonttoolsConverter", "ConverterInterface"]
+__all__ = ["FFmpegConverter", "PillowConverter", "PandasConverter", "DrawioConverter", "PyPandocConverter", "PyMuPDFConverter", "PySubs2Converter", "FonttoolsConverter", "LibreOfficeConverter", "ConverterInterface"]
